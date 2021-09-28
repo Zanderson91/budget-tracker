849b5a4cebe5870755db5891add9d1c5ee45cf5d
Zack Anderson <zack.anderson2011@gmail.com>
2021-09-28 07:26:02 -0500

    continuing conditional logic to return data in json and store

--- develop/public/db.js
+++ develop/public/db.js
@@ -3,7 +3,9 @@ const request = index.db.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    db.createObjectStore("pending", {autoIncrement: true});
+    db.createObjectStore("pending", {
+        autoIncrement: true
+    });
 };
 
 request.onsuccess = function (event) {
@@ -31,14 +33,20 @@ function checkData() {
     getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
             fetch("/api/transaction/all", {
-                method: "POST",
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json"
-                }
-            })
-        }
-    }
+                    method: "POST",
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                        "Content-Type": "application/json"
+                    }
+                })
 
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
 }
